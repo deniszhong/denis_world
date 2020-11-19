8c185a1d2fe0e8812f544cadfa451731d838e718
zhongzf <deniszhong@126.com>
2020-11-19 10:40:55 +0800

    1.spring-security

--- src/test/java/com/denis/zhong/world/controller/TopicControllerTest.java
+++ src/test/java/com/denis/zhong/world/controller/TopicControllerTest.java
@@ -1,7 +1,23 @@
 package com.denis.zhong.world.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.denis.zhong.world.common.BaseTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 class TopicControllerTest extends BaseTest {
 
+    @Test
+    public void selectOne() throws Exception{
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/topic/selectOne/1")
+                .contentType(MediaType.APPLICATION_JSON);
+        ResultActions actions = this.mockMvc.perform(requestBuilder);
+        MockHttpServletResponse response = actions.andReturn().getResponse();
+        System.out.println(JSON.toJSONString(response));
+    }
 }
